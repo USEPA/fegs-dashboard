a46b8a0dac6fbbc87fc1f6858f33b0b3082f9acb
Kyle Thomas <wetlife@gmail.com>
2016-09-20 17:48:35 -0700

    WIP plot in param-space attrs x bens with bokeh

--- dev/smatterplot/color_smatter.py
+++ dev/smatterplot/color_smatter.py
@@ -1,35 +1,27 @@
 import numpy as np
-
+import os
+import pandas as pd
 from bokeh.plotting import figure, show, output_file
 
-# circle(attr_i, ben_i) draws a circle at (attr_i, ben_i)
-# draw circles at each grid-point
-# load csv of attrs
-# - use csv.reader or pd.read_csv()
-# make plot-axis with attrs on it
-# load csv of bens
-# make plot-axis with bens on it
-# - bokeh.model.axes.Axis.CategoricalAxis is the class to use
-# - 
-# map rating-values: "Good"|->1, "Fair"|->0, "Poor"|->-1
-# load csv of ratings
-# R = set of all k ratings
-# count the number of ratings, k_i, for plot-point p_i in space S = attrs x bens
+
 # R_i = set of all k_i ratings of (ben_i, attr_i)
+# count the number of ratings, k_i, for plot-point p_i in space S = attrs x bens
 # calculate E(p_i) = SUM_j(r_j)/k
 # make a map [0,1] -> Saturation s.t. E(p_i) |-> <saturation> 
-# w(p_i) = k_i/k is in [0,1] for all i<k
+# weight(p_i) = k_i/k is in [0,1] for all i<k
+# draw circles at each grid-point
+# - circle(attr(i), ben(j)) draws a circle at (attr_i, ben_j)
 
-N = 40
-dx = 20
-dy = 20
+
+N = 240
+dx = 50
+dy = 50
 # load attrs from attrs.csv
 # load bens from bens.csv
-# x = np.array(len(attrs))
 x = np.random.random(size=N) * dx
-# y = np.array(len(bens))
 y = np.random.random(size=N) * dy
-# S = ((attr, ben) for attr in attrs for ben in bens((x, y) for x in a for y in b))
+# (x,y) = [(x,y) for (x,y) in points]
+# points = ((attr, ben) for attr in attrs for ben in bens((x, y) for x in a for y in b))
 # S[(attr, ben)][k] = <numratings>
 # radii \prop k_i
 radii = np.random.random(size=N) * 1.5
