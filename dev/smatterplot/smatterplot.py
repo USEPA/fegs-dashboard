72d1e1249abfc1d83f36c5b2c75ac1627caa5ab1
wetlife <wetlife@gmail.com>
2016-09-22 09:35:56 -0700

    check in

--- dev/smatterplot/smatterplot.py
+++ dev/smatterplot/smatterplot.py
@@ -2,7 +2,6 @@
 # imports #
 ###########
 from bokeh.plotting import figure, show, output_file
-from bokeh.charts import Scatter
 import pandas as pd
 import numpy as np
 import os
@@ -11,7 +10,8 @@ import pdb
 ####################
 # parametrizations #
 ####################
-homedir = r'c:\\users\\kthom02' # environmental parametrization
+# environmental parametrizations
+homedir = r'/home/thomasky/Downloads' #r'c:\\users\\kthom02'
 toolpath = os.path.join(homedir,'fegs-dashboard')
 myglob = os.path.join(toolpath, 'ratings', "*.csv")
 all_files = glob.glob(myglob)
@@ -19,8 +19,7 @@ all_files = glob.glob(myglob)
 # load csv of attrs #
 #####################
 attrsfilename = os.path.join(
-        homedir,
-        'fegs-dashboard',
+        toolpath,
         'parameters',
         'attributes.csv')
 attrsdf = pd.read_csv(attrsfilename)
@@ -29,8 +28,7 @@ attrs = list(attrsdf.name)
 # load csv of bens #
 ####################
 benspath = os.path.join(
-        homedir,
-        'fegs-dashboard',
+        toolpath,
         'parameters',
         'beneficiaries.csv')
 bensdf = pd.read_csv(benspath)
@@ -52,16 +50,16 @@ goupedratings = ratingsdf.groupby(['Attribute', 'Beneficiary'])
 ########
 # plot #
 ########
-attrs = list(ratingsdf.Attribute)
-bens = list(ratingsdf.Beneficiary)
-p = figure()
-p.circle(   
-            xlist=attrs,
-            ylist=bens,
+attrsvals = list(ratingsdf.Attribute)
+bensvals = list(ratingsdf.Beneficiary)
+p = figure(x_range=attrs, y_range=bens)
+p.circle(   x=attrsvals,
+            y=bensvals,
             radius=10,#radius=f(weight(p))
             #fill_color=,
             line_color=None,
             fill_alpha=0.5)
+show(p)
 
 # R_i = set of all k_i ratings of (ben_i, attr_i)
 # count the number of ratings, k_i, for plot-point p_i in space S = attrs x bens
