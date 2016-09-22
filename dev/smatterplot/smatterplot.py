54b8e143fdd56f2eec3265712de6246db76b3763
Kyle Thomas <wetlife@gmail.com>
2016-09-21 17:23:56 -0700

    close to plotting

--- dev/smatterplot/smatterplot.py
+++ dev/smatterplot/smatterplot.py
@@ -2,35 +2,46 @@
 # imports #
 ###########
 from bokeh.plotting import figure, show, output_file
+from bokeh.charts import Scatter
 import pandas as pd
 import numpy as np
 import os
 import glob
+import pdb
 ####################
 # parametrizations #
 ####################
-toolpath = os.path.join(r'~','fegs-dashboard')
+homedir = r'c:\\users\\kthom02' # environmental parametrization
+toolpath = os.path.join(homedir,'fegs-dashboard')
 myglob = os.path.join(toolpath, 'ratings', "*.csv")
 all_files = glob.glob(myglob)
 #####################
 # load csv of attrs #
 #####################
-attrsfilename = os.path.join(r'~', 'fegs-dashboard', 'parameters', 'attributes.csv')
+attrsfilename = os.path.join(
+        homedir,
+        'fegs-dashboard',
+        'parameters',
+        'attributes.csv')
 attrsdf = pd.read_csv(attrsfilename)
 attrs = list(attrsdf.name)
 ####################
 # load csv of bens #
 ####################
-benspath = r'~/fegs-dashboard/parameters/beneficiaries.csv'
+benspath = os.path.join(
+        homedir,
+        'fegs-dashboard',
+        'parameters',
+        'beneficiaries.csv')
 bensdf = pd.read_csv(benspath)
 bens = list(bensdf.name)
 ###########################################
 # make figure with attrs,bens on the axes #
 ###########################################
 p = figure(x_range=attrs, y_range=bens)
-############################################################
-# map rating-values: {"Good"|->1, "Fair"|->0, "Poor"|->-1} #
-############################################################
+#####################
+# map rating-values #
+#####################
 values = {"Good":1, "Fair":0, "Poor":-1}
 # kth_rating_value = values[ratings[k]]
 ##################################################
@@ -41,14 +52,16 @@ goupedratings = ratingsdf.groupby(['Attribute', 'Beneficiary'])
 ########
 # plot #
 ########
-xlist = list(df.groupby('Attribute'))
-ylist = list(df.groubby('Beneficiary'))
-scatter(xlist=attrs,
-        ylist=bens,
-        radius=10,#radius=f(weight(p))
-        #fill_color=,
-        line_color=None,
-        fill_alpha=0.5)
+attrs = list(ratingsdf.Attribute)
+bens = list(ratingsdf.Beneficiary)
+p = figure()
+p.circle(   
+            xlist=attrs,
+            ylist=bens,
+            radius=10,#radius=f(weight(p))
+            #fill_color=,
+            line_color=None,
+            fill_alpha=0.5)
 
 # R_i = set of all k_i ratings of (ben_i, attr_i)
 # count the number of ratings, k_i, for plot-point p_i in space S = attrs x bens
