67f672a23a454d44e000846dff4219359c64358a
Kyle Thomas <wetlife@gmail.com>
2018-02-06 14:26:17 -0800

    BDD on display: $ npm install && npm run test
    implement proposals, which look like it("text")

--- test/spec.js
+++ test/spec.js
@@ -2,11 +2,18 @@ const Application = require('spectron').Application
 const assert = require('assert')
 const electronPath = require('electron') // Require Electron from the binaries included in node_modules.
 const path = require('path')
- 
-describe('Isolation testbed', function () {
-  this.timeout(30000)
- 
-  beforeEach(function () {
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+
+describe('Isolation testbeds', function() {
+  this.timeout(30000);
+
+  global.before(function() {
+    chai.should();
+    chai.use(chaiAsPromised);
+  });
+
+  before(function() {
     this.app = new Application({
       // Your electron path can be any binary
       // i.e for OSX an example path could be '/Applications/MyApp.app/Contents/MacOS/MyApp'
@@ -29,28 +36,44 @@ describe('Isolation testbed', function () {
       args: [path.join(__dirname, '..')]
     })
     return this.app.start()
-  })
- 
-  afterEach(function () {
-    if (this.app && this.app.isRunning()) {
-      return this.app.stop()
-    }
-  })
- 
-  it('shows an initial window', function () {
+  });
+
+  it('shows an initial window', function() {
     return this.app.client.getWindowCount().then(function (count) {
       assert.equal(count, 1)
       // Please note that getWindowCount() will return 2 if `dev tools` are opened.
       // assert.equal(count, 2)
     })
-  })
+  });
 
-  it('shows project name by app\'s title')
-  it('allows users to change project name where it\'s shown')
+  it('scrapes view', function() {
+    return this.app.client.getValue('#magnitude-score').should.become('1');
+  });
 
-  it('saves to a file')
+  it('saves to a file', function() {
+    var fs = require('fs');
+    return this.app.client.getValue('fegsScopingData').should.eventually.contain('criteria');
+  });
 
-  it('opens from a file')
+  it('opens from a file');
 
-  it('hides unrepresented beneficiaries automatically')
+  it('hides unrepresented beneficiaries automatically');
+
+  it('style app with EPA\'s web-style');
+
+  it('shows project-name as app\'s title', function() {
+    this.app.client.waitUntilWindowLoaded()
+    this.app.client.document.title = 'foo';
+    return this.app.client.waitUntilWindowLoaded()
+      .getTitle().should.eventually.equal('FEGS Scoping Tool');
+  });
+
+  it('allows users to change project name where it\'s shown');
+
+  after(function() {
+    if (this.app && this.app.isRunning()) {
+      return this.app.stop()
+    }
+  });
+
 })
