8b9709050f93d63f46ab9f3f0b6ed7a8254a0859
Kyle Thomas <wetlife@gmail.com>
2018-03-07 17:11:05 -0800

    2018.03.07 show work in WIP-state

--- test/spec.js
+++ test/spec.js
@@ -1,18 +1,17 @@
-const Application = require('spectron').Application
-const assert = require('assert')
-const electronPath = require('electron') // Require Electron from the binaries included in node_modules.
-const path = require('path')
+const Application = require('spectron').Application;
+const electronPath = require('electron'); // Require Electron from the binaries included in node_modules.
+const path = require('path');
 const chai = require('chai');
+const { assert } = require('chai');
 const chaiAsPromised = require('chai-as-promised');
 const fs = require('fs');
+const should = require('chai').should();
+const sinon = require('sinon');
+
+chai.use(chaiAsPromised);
 
 describe('Isolated testbeds house suites of tests which are certainly independent.', function() {
   this.timeout(30000);
-
-  global.before(function() {
-    chai.should();
-    chai.use(chaiAsPromised);
-  });
 
   before(function() {
     this.app = new Application({
@@ -45,37 +44,77 @@ describe('Isolated testbeds house suites of tests which are certainly independen
     }
   });
 
-  it('show an initial window', function() {
+  /* THIS COMMENT DOCUMENTS USAGE OF THE TESTING-FRAMEWORK
+  it('tests access to variables from embedded script within HTML the application', function() {
+    return this.app.foo == 'foo'; // manually assert global variable named foo has value 'foo'
+  });
+
+  it('tests behavior obvious to any user in true BDD-style', function() {
+    return this.app.client.click('#getStuffAsynchronously').element('#elementCreatedToContainDataReturnedAsynchronously').should.eventually.exist; // demonstrates BDD with promise-handling
+  });
+  */
+
+  it('shows an initial window', function() {
     return this.app.client.getWindowCount().then(function (count) {
-      assert.equal(count, 1)
+      assert.equal(count, 1);
       // Please note that getWindowCount() will return 2 if `dev tools` are opened.
       // assert.equal(count, 2)
     })
   });
 
-  it('scrape view', function() {
-    // scrape page, save data to #scrape-page.data-view-state, and getAttribute(#scrape-page, data-view-state)
-    //TODO FIXME by finishing
-    require('fs').rename('data.json', 'data-renamed.json') // given no data.json
+  it('shows project-name as app\'s title', function() {
+    return Promise.resolve(this.app.client.getTitle()).should.eventually.equal('FEGS Scoping Tool');
+  });
+
+  it('allows users to change project name where it\'s shown', function() {
+    return this.app.client.click('#change-name').element('#input-name').setValue('42').click('#save-name').getTitle().should.eventually.equal('42');
+  });
+
+  describe('given weights, stakeholder-name, and stakeholder-weights have been entered', function() {
+    before( function() {
+      // populate inputs in #table-scores with ones to support later calculation
+      var i;
+      var document = this.app.client.document;
+      var inputs = document.querySelector('#table-scores input');
+      for (i = 0; i < inputs.length; i++) {
+        inputs[i].value = '1';
+      }
+      document.getElementById('stakeholder-group').value = 'foo';
+      document.querySelector('#add-stakeholder').click();
+      inputs = document.querySelector('#set-stakeholder-values input');
+      for (i = 0; i < inputs.length; i++) {
+        inputs[i].value = '1';
+      }
+      document.querySelector('#set-stakeholder-values button').click();
+      document.querySelector('#select-stakeholder-to-slice-into-beneficiaries').value = 'foo';
+      document.querySelector('#select-stakeholder-to-slice-into-beneficiaries').onchange();
+    });
+    describe('when valid data are entered into input.beneficiary-percentage-of-stakeholder', function() {
+      before( function() {
+        document.querySelectorAll('.beneficiary-percentage-of-stakeholder')[0].value = '100';
+        var spy = sinon.spy(tableAttributes.showOnlyTheseColumns);
+        document.querySelectorAll('.beneficiary-percentage-of-stakeholder')[0].onchange();
+      };
+      it('then unrepresented beneficiaries are hidden within #table-attributes', function() {
+        return spy.called()//FIXME assert call of tableAttributes.showOnlyTheseColumns(fegsScopingData.extantBeneficiaries())
+      });
+    });
+  });
+
+  it('hides unrepresented beneficiaries automatically', function() {
+    return this.app.client.waitUntilWindowLoaded();
+  });
+
+  it('saves state of application to a file', function() { // scrape page, save data to #scrape-page.data-view-state, and getAttribute(#scrape-page, data-view-state)
+    if (require('fs').exists('data.json')) { // given there is no data.json
+      require('fs').rename('data.json', 'data-renamed.json');
+    }
     this.app.client.click('#scrape-page'); // when #scrape-page.click()
-    return Promise.resolve(require('fs').existsSync('data.json')).should.eventually.be.true; // then data.json should be true
+    return require('fs').exists('data.json') // then data.json should be true
   });
 
-  it('save to a file', function() {
-    return this.app.client.getValue('fegsScopingData').should.eventually.contain('criteria');
-  });
+  it('opens from a file');
 
-  it('open from a file');
-
-  it('hide unrepresented beneficiaries automatically');
-
-  it('style app with EPA\'s web-style');
-
-  it('show project-name as app\'s title', function() {
-    return this.app.client.waitUntilWindowLoaded()
-      .getTitle().should.eventually.equal('FEGS Scoping Tool');
-  });
-
-  it('allow users to change project name where it\'s shown');
+  it('styles app with EPA\'s web-style');
 
 })
